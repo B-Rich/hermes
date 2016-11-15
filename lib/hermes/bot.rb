0cf7da0caef98833956a353971d9295d3fdc5a99
Abraham Kuri <kurenn@icalialabs.com>
2016-11-15 12:36:00 -0600

    Integrates Olimpo to handle authentication & connection

--- lib/hermes/bot.rb
+++ lib/hermes/bot.rb
@@ -1,22 +1,9 @@
 require "hermes/bot/version"
-require "active_support/dependencies"
-require "httmultiparty"
+require "olimpo"
 
 module Hermes
   module Bot
+    extend Olimpo
     autoload :Translator, "hermes/bot/translator"
-
-    # Username for authentication
-    mattr_accessor :username
-    @@username = nil
-
-    # Password for authentication
-    mattr_accessor :password
-    @@password = nil
-
-    # Method to set the authentication keys for the bot
-    def self.configure
-      yield self
-    end
   end
 end
--- lib/hermes/bot/translator.rb
+++ lib/hermes/bot/translator.rb
@@ -1,13 +1,10 @@
-require "hermes/bot/connection"
-require "hermes/bot/errors"
 require "hermes/bot/identifiable_language"
 require "hermes/bot/language"
 require "hermes/bot/model"
 
 module Hermes
   module Bot
-    class Translator
-      include Hermes::Bot::Connection
+    class Translator < Olimpo::Base
 
       def self.translate(params = {})
         response = post("/v2/translate", body: params)
@@ -75,13 +72,6 @@ module Hermes
         return Hermes::Bot::Model.new(parsed_response) if response.success?
 
         raise_exception(response.code, response.body)
-      end
-
-      private
-
-      def self.raise_exception(code, body)
-        raise Hermes::Bot::ServerError.new(code, body) if code >= 500
-        raise Hermes::Bot::ClientError.new(code, body) if code < 500
       end
     end
   end
