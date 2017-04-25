7aa79f1e0793dab43afc7e79574ce9dd04b6111d
Jelizondo <jelizondolevy.s@gmail.com>
2017-04-25 10:21:58 -0500

    Starts with bot_spec tests

--- spec/hermes/bot_spec.rb
+++ spec/hermes/bot_spec.rb
@@ -5,7 +5,37 @@ describe Hermes::Bot do
     expect(Hermes::Bot::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
+
+  it 'translator is working' do
+    Hermes::Bot.configure do |config|
+	     config.username = "174f2d4d-4130-4d0d-8641-448646347489"
+	     config.password = "u72xDYFm4ycO"
+	     config.base_uri = "https://gateway.watsonplatform.net/language-translator/api"
+    end
+    expect(Hermes::Bot::Translator.translate(source: "en", target: "es", text: "hello")).to eq("Hola ")
   end
+
+  # it 'identifiable_languages is working' do
+  #
+  # end
+  #
+  # it 'identify a language is word' do
+  #
+  # end
+  # 
+  # it 'models is working' do
+  #
+  # end
+  #
+  # it 'identify a model is working' do
+  #
+  # end
+  #
+  # it 'creating a model is working' do
+  #
+  # end
+
 end
